08348193acdc4fba751c4f40a9f2ca80eb19f012
sarzixon <msarzala.work@gmail.com>
2025-03-21 13:35:40 +0100

    fixed scroll to top

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,14 +1,18 @@
 
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 /**
  * ScrollToTop component that scrolls the window to the top when the route changes
  */
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
+    if (hash.length) {
+      return;
+    }
+
     window.scrollTo(0, 0);
   }, [pathname]);
 
